43983b9a24a64df658c270fcd7d2014f19ba6c1a
hyyyyyyz <1783866380@qq.com>
2025-08-18 18:01:30 +0800

    update on 2025.8.18

---
+++ configs/__init__.py
@@ -0,0 +1,5 @@
+from .base_config import *
+from .legged_robot_config import *
+from .go2_constraint_him import *
+from .go2_constraint_trans_p1 import *
+from .go2_constraint_trans_p2 import *
---
+++ depoly/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/pi/Downloads/LocomotionWithNP3O-master/depoly/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/noetic/share/euslisp/jskeus/eus//Linux64/lib:/home/pi/Downloads/LocomotionWithNP3O-master/depoly/devel/lib:/home/pi/catkin_ws/devel/lib:/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu:/opt/ros/noetic/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/noetic/share/euslisp/jskeus/eus//Linux64/lib'
+export PATH='/opt/ros/noetic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/noetic/bin:/opt/ros/noetic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/noetic/share/euslisp/jskeus/eus//Linux64/bin:/home/pi/anaconda3/condabin:/home/pi/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH="/home/pi/Downloads/LocomotionWithNP3O-master/depoly/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/pi/Downloads/LocomotionWithNP3O-master/depoly/build'
+export PYTHONPATH="/home/pi/Downloads/LocomotionWithNP3O-master/depoly/devel/lib/python3/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/pi/Downloads/LocomotionWithNP3O-master/depoly/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH='/home/pi/Downloads/LocomotionWithNP3O-master/depoly/src:/home/pi/catkin_ws/src:/opt/ros/noetic/share'
