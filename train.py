a4cc1fe894c40984535061f5f30b7bf510de26fa
hyyyyyyz <1783866380@qq.com>
2025-08-27 21:57:05 +0800

    delete two demos

--- train.py
+++ train.py
@@ -2,8 +2,6 @@ import numpy as np
 import os
 from datetime import datetime
 from configs.go2_constraint_him import Go2ConstraintHimRoughCfg, Go2ConstraintHimRoughCfgPPO
-from configs.go2_constraint_trans_p1 import Go2ConstraintTransP1RoughCfg, Go2ConstraintTransP1RoughCfgPPO
-from configs.go2_constraint_trans_p2 import Go2ConstraintTransP2RoughCfg, Go2ConstraintTransP2RoughCfgPPO
 from configs.tinymal_constraint_him import TinymalConstraintHimRoughCfg, TinymalConstraintHimRoughCfgPPO
 import isaacgym
 from utils.helpers import get_args
@@ -20,8 +18,7 @@ def train(args):
 if __name__ == '__main__':
     task_registry.register("go2N3poHim",LeggedRobot,Go2ConstraintHimRoughCfg(),Go2ConstraintHimRoughCfgPPO())
     task_registry.register("Tinymal",LeggedRobot,TinymalConstraintHimRoughCfg(),TinymalConstraintHimRoughCfgPPO())
-    task_registry.register("go2N3poTransP1",LeggedRobot,Go2ConstraintTransP1RoughCfg(),Go2ConstraintTransP1RoughCfgPPO())
-    task_registry.register("go2N3poTransP2",LeggedRobot,Go2ConstraintTransP2RoughCfg(),Go2ConstraintTransP2RoughCfgPPO())
+
     args = get_args()
     # args.task='Tinymal'
     train(args)
